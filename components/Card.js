eb33ed92674c609109308764efff88cf28e70e64
SuNgu <cliffland.jasungu@gmail.com>
2025-08-17 12:55:20 +0300

    check spelling

--- components/Card.js
+++ components/Card.js
@@ -1,7 +1,7 @@
 export default function Card({ title, img }) {
     return (
         <div className="rounded-xl overflow-hidden shadow-md hover:scale-105 transform transition duration-300">
-            <img 
+            <image 
                 src={img} 
                 alt={title} 
                 className="w-full h-40 object-cover" 
